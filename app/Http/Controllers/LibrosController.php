0bce68609a2d8807d09aecc7fc6b2610494b7dd4
jonathangoyes96 <jonathangoyes96@gmail.com>
2018-03-31 23:03:35 -0500

    functions and views added

--- app/Http/Controllers/LibrosController.php
+++ app/Http/Controllers/LibrosController.php
@@ -3,49 +3,68 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Libro;
 
 class LibrosController extends Controller
 {
+
     /**
      * Display a listing of the resource.
-     *
+     * Mostrando los datos de los libros almacenados en la base de datos
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
         //
+        $libros=Libro::orderBy('id','DESC')->paginate(3);
+        return view('libros.index',['libros' => $libros]);
     }
 
     /**
      * Show the form for creating a new resource.
-     *
+     * Muestra el formulario de creacion de un nuevo libro en la base de datos
      * @return \Illuminate\Http\Response
      */
     public function create()
     {
         //
+        return view('libros.create');
     }
 
     /**
      * Store a newly created resource in storage.
-     *
+     * 1. Valida que los campos que se ingresen en el formulario no esten vacios
+     * 2. almacena el la base de datos el nuevo libro creado
+     * 3. Redirige al usuario a la pantalla principal con un mensaje de exito
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         //
+        $this->validate($request,[
+            'nombre'=>'required',
+            'resumen'=>'required',
+            'npagina'=>'required',
+            'edicion'=>'required',
+            'autor'=>'required',
+            'npagina'=>'required',
+            'precio'=>'required'
+        ]);
+        Libro::create($request->all());
+        return redirect()->route('libros.index')->with('success','Registro creado satisfactoriamente');
     }
 
     /**
      * Display the specified resource.
-     *
+     * Muestra los detalles de un libro
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        $libros=Libro::find($id);
+        return  view('libros.show',compact('libros'));
     }
 
     /**
@@ -57,6 +76,8 @@ class LibrosController extends Controller
     public function edit($id)
     {
         //
+        $libro=libro::find($id);
+        return view('libros.edit',compact('libro'));
     }
 
     /**
@@ -66,9 +87,13 @@ class LibrosController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id)    {
         //
+        $this->validate($request,[ 'nombre'=>'required', 'resumen'=>'required', 'npagina'=>'required', 'edicion'=>'required', 'autor'=>'required', 'npagina'=>'required', 'precio'=>'required']);
+
+        libro::find($id)->update($request->all());
+        return redirect()->route('libros.index')->with('success','Registro actualizado satisfactoriamente');
+
     }
 
     /**
@@ -80,5 +105,7 @@ class LibrosController extends Controller
     public function destroy($id)
     {
         //
+        Libro::find($id)->delete();
+        return redirect()->route('libros.index')->with('success','Registro eliminado satisfactoriamente');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+ * GET /libro -> muestra todos los libros
+ * GET /libro/create -> muestra formulario de creacion de un nuevo libro
+ * GET /libro/edit -> muestra formulario de edicion de un libro
+ * GET /libro/{id} -> muestra un libro por id
+ * POST /libro -> Crea un nuevo libro en la base de datos
+ * PUT/PATCH /libro/{id} -> actualiza un libro
+ * DELETE /libro/{id} -> elimina un libro
+ */
+Route::resource('libro', 'LibrosController');
